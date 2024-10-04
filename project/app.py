82abc2fe1329a866218ba196b315685f55f52731
W <111629334+wth-d@users.noreply.github.com>
2024-10-04 13:16:22 -0400

    step11 done - login required

--- project/app.py
+++ project/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pathlib import Path
+from functools import wraps
 
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
@@ -25,6 +26,17 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models # put here to avoid circular import??
+
+
+# a decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -71,6 +83,7 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
